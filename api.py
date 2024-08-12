59ebf39eee453c79a65e265c30b31eab41b7f669
ckfejrdld <70272129+ckfejrdld@users.noreply.github.com>
2024-08-12 17:00:05 +0900

    Added verifying domain.

--- api.py
+++ api.py
@@ -67,3 +67,9 @@ def create_backend(network_name: str, backend_name: str, hostname: str, port: in
     body = {"name": backend_name, "backends": [f'{hostname}:{port}'], "proxy_protocol": proxyprotocol, "vulcan_ac_enabled": False, "load_balancing_mode": 0}
     response = requests.post(f"{url}/networks/{network_id}/backendSets", headers=headers, json=body)
     return response.status_code
+
+def verify_domain(network_name: str, domain:str):
+    network_id = get_network_id(network_name)
+    domain_id = get_domain_id(network_name, domain)
+    response = requests.get(f"{url}/networks/{network_id}/domains/{domain_id}/verify", headers=headers)
+    return response.status_code # 200 - 정상 / 403: 인증 실패
